276c3427ea75e493049e83ee73ae79f1914d58b1
EricSites <eric_sites@mac.com>
2012-03-08 00:29:30 -0500

    Update for Rails 3.2

--- lib/vestal_versions/deletion.rb
+++ lib/vestal_versions/deletion.rb
@@ -9,31 +9,26 @@ module VestalVersions
     end
 
     # Class methods on ActiveRecord::Base
-    module ClassMethods
-      # After the original +prepare_versioned_options+ method cleans the given options, this alias
-      # also extracts the <tt>:depedent</tt> if it set to <tt>:tracking</tt>
-      def prepare_versioned_options(options)
-        result = super(options)
-        if result[:dependent] == :tracking
-          self.vestal_versions_options[:track_destroy] = true
-          options.delete(:dependent)
-        end
-
-        result
+    # After the original +prepare_versioned_options+ method cleans the given options, this alias
+    # also extracts the <tt>:depedent</tt> if it set to <tt>:tracking</tt>
+    def prepare_versioned_options(options)
+      result = super(options)
+      if result[:dependent] == :tracking
+        self.vestal_versions_options[:track_destroy] = true
+        options.delete(:dependent)
       end
+
+      result
     end
 
-    module InstanceMethods
-      private
+    private
 
-        def delete_version?
-          vestal_versions_options[:track_destroy]
-        end
+      def delete_version?
+        vestal_versions_options[:track_destroy]
+      end
 
-        def create_destroyed_version
-          create_version(version_attributes.merge(:modifications => attributes, :tag => 'deleted'))
-        end
-
-    end
+      def create_destroyed_version
+        create_version(version_attributes.merge(:modifications => attributes, :tag => 'deleted'))
+      end
   end
 end
